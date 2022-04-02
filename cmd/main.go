70fba3ab83b844522d649b039eb3216a6b915af8
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-02 11:58:39 +0300

    Calling database connect, auto migrate and init data functions in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/enspzr/nba-simulation/data"
+	"github.com/enspzr/nba-simulation/storage/postgre"
+)
 
 func main() {
 	fmt.Println("Backend Starting")
+	err := postgre.Connect()
+	if err != nil {
+		panic("Database connection failed => " + err.Error())
+	}
+	postgre.AutoMigrate()
+	data.InitData()
+	fmt.Println("Backend Started")
 }

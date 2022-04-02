c67c131c49ca662b8b42f28b081a72482339f206
Mehmet Enes PAZAR <m.enespazar@gmail.com>
2022-04-02 11:53:15 +0300

    Added helpers functions for team

--- model/team.go
+++ model/team.go
@@ -9,3 +9,19 @@ type Team struct {
 
 	Players []Player `json:"players"`
 }
+
+func GetTeamsIds(teams []Team) []int {
+	ids := make([]int, 0)
+	for _, team := range teams {
+		ids = append(ids, team.ID)
+	}
+	return ids
+}
+
+func TeamsToMap(teams []Team) map[int]Team {
+	teamsMap := make(map[int]Team)
+	for _, team := range teams {
+		teamsMap[team.ID] = team
+	}
+	return teamsMap
+}
